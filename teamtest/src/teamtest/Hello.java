4347baf2deba11cb9fe93c5ed1a82dbda2ed1374
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-25 10:20:36 +0900

    문제해결완료

--- teamtest/src/teamtest/Hello.java
+++ teamtest/src/teamtest/Hello.java
@@ -2,8 +2,11 @@ package teamtest;
 
 public class Hello {
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		//로컬에서 작업했습니다.
+=======
 		//원격에서 작업했습니다.
+branch 'master' of https://github.com/lazulikim/myteam.git
 		System.out.println("Hello world");
 		
 		System.out.println("변경");
